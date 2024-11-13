6cb9f61c94aa929ada0653be486fa80bcd4d3e6b
paulagv05 <paula.garcia.valls@estudiantat.upc.edu>
2024-11-13 13:15:00 +0100

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,19 +1,52 @@
-// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <algorithm> 
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+   int* array = nullptr;     
+    int size = 0;            
+    int capacity = 0;     
+
+    while (true) {
+        int num;
+       cout << "Introduce un número (o -1 para salir): ";
+       cin >> num;
+
+        if (num == -1) {
+            cout << "\nAdios muy buenas!";
+            break;  
+        }
+
+        
+        if (size == capacity) {
+            
+            int* newArray = new int[capacity];  
+            for (int i = 0; i < size; ++i) 
+            {
+                newArray[i] = array[i];
+            }
+
+            delete[] array;  
+            array = newArray;  
+        }
+        array[size++] = num;
+
+        
+        sort(array, array + size);
+
+        cout << "Array actual: [";
+        for (int i = 0; i < size; ++i) 
+        {
+            cout << array[i];
+            if (i < size - 1)
+            {
+               cout << ", ";
+            };
+        }
+        cout << "]" << endl;
+    }
+  
+    delete[] array;
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
